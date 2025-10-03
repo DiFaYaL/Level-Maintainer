8266ae55f020e4dbe97be0bcdbf5442457687c36
DiFaYaL <vkolobchuk@yandex.ru>
2025-10-03 08:50:32 +0300

    Update config.lua

--- config.lua
+++ config.lua
@@ -4,15 +4,10 @@ local cfg = {}
 
 -- [item_name] = {threshold, batch_size} -- keep in mind that no threshold has a better performance!
 -- ["Osmium Dust"] = {nil, 64} -- without threshold, batch_size=64
--- ["drop of Molten SpaceTime"] = {1000000, 1} -- with threshold
-
+-- ["drop of Molten SpaceTime"] = {1000, 1, "molten_spacetime"} -- with threshold
 
 cfg["items"] = {
-  ["Osmium Dust"] = {256, 64},
-  ["drop of Molten Polybenzimidazole"] = {100, 1, "molten_polybenzimidazole"},
-  ["drop of Americium Plasma"] = {100, 1, "americium_plasma"},
-  ["Phosphorus Dust"] = {256, 64},
-  ["drop of Phosphoric Acid"] = {100, 1, "phosphoric_acid"},
+
 }
 
 cfg["sleep"] = 10
