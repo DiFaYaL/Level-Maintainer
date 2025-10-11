5668ead9ba0f6e9f37746e9c4bc527078f4701a0
DiFaYaL <vkolobchuk@yandex.ru>
2025-10-11 15:55:29 +0300

    add new format for improved performance in large AE networks

--- config.lua
+++ config.lua
@@ -2,9 +2,10 @@ local cfg = {}
 
 -- EXAMPLE --
 
--- [item_name] = {threshold, batch_size} -- keep in mind that no threshold has a better performance!
--- ["Osmium Dust"] = {nil, 64} -- without threshold, batch_size=64
--- ["drop of Molten SpaceTime"] = {1000, 1, "molten_spacetime"} -- with threshold
+-- ["item_name"] = {{item_id, item_meta},threshold, batchsize} -- keep in mind that no threshold has a better performance!
+-- ["fluid_name"] = {{fluid_tag item},threshold, batchsize}}  -- keep in mind that no threshold has a better performance!
+-- ["Osmium Dust"] = {{ item_id = "gregtech:gt.metaitem.01", item_meta = "2083"}, 1000, 64}, -- without threshold, batch_size=64
+-- ["drop of Molten SpaceTime"] = {{ fluid_tag = "molten.spacetime"}, 1000, 64} -- with threshold, batch_size=64
 
 cfg["items"] = {
 
