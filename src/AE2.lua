1840cbb24439af923f2180d228e2dabfa9e7fe66
DiFaYaL <vkolobchuk@yandex.ru>
2025-10-11 15:42:19 +0300

    improved performance in large AE networks

--- src/AE2.lua
+++ src/AE2.lua
@@ -7,25 +7,31 @@ local itemCache = {}
 local cacheTimestamp = 0
 local CACHE_DURATION = 600 -- 10 минут
 
+
 local function formatNumber(num)
     if type(num) ~= "number" then return tostring(num) end
     local str = tostring(num)
     local parts = {}
     local len = #str
     local firstGroup = len % 3
+    
     if firstGroup == 0 then firstGroup = 3 end
+    
     table.insert(parts, str:sub(1, firstGroup))
+    
     local i = firstGroup + 1
+    
     while i <= len do
+        
         table.insert(parts, str:sub(i, i + 2))
         i = i + 3
+    
     end
     return table.concat(parts, "_")
 end
 
 local function getCraftableForItem(itemName)
     local currentTime = os.time()
-
     if currentTime - cacheTimestamp >= CACHE_DURATION then
         itemCache = {}
         cacheTimestamp = currentTime
@@ -37,11 +43,13 @@ local function getCraftableForItem(itemName)
 
     local craftables = ME.getCraftables({label = itemName})
     local craftable = craftables[1]
+    
     itemCache[itemName] = craftable
+    
     return craftable
 end
 
-function AE2.requestItem(name, threshold, count)
+function AE2.requestItem(name, data, threshold, count)
     local craftable = getCraftableForItem(name)
     if not craftable then
         return false, "is not craftable!"
@@ -51,15 +59,23 @@ function AE2.requestItem(name, threshold, count)
     local itemInSystem = nil
 
     if threshold then
-        local itemsInSystem = ME.getItemsInNetwork({label = name})
-        itemInSystem = itemsInSystem[1]
+        if (data.fluid_tag ~= nil) then
+            itemInSystem = ME.getItemInNetwork('ae2fc:fluid_drop',0, "{Fluid:\"" .. data.fluid_tag .. "\"}")
 
-        if itemInSystem and itemInSystem.size >= threshold then
+        else
+
+            itemInSystem = ME.getItemInNetwork(data.item_id,tonumber(data.item_meta))
+        end
+
+        if  itemInSystem and itemInSystem.size >= threshold then
+
             local currentAmount = formatNumber(itemInSystem.size)
             local thresholdFmt = formatNumber(threshold)
             return false, "The amount (" .. currentAmount .. ") threshold (" .. thresholdFmt .. ")! Aborting request."
+
         end
-    end
+
+end
 
     if item.label == name then
         local craft = craftable.request(count)
@@ -78,11 +94,16 @@ end
 function AE2.checkIfCrafting()
     local items = {}
     for _, cpu in pairs(ME.getCpus()) do
+        
         local final = cpu.cpu.finalOutput()
+        
         if final then
+            
             items[final.label] = true
+        
         end
     end
+    
     return items
 end
 
